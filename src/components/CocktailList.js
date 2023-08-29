47fe0cd1920f7875bb6553ad4769cfaf1acc9073
unknown <anands2807@gmail.com>
2023-08-29 10:01:13 +0530

    initial setup doen, all components and pages structure created.

---
+++ src/components/CocktailList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CocktailList() {
+  return (
+    <div>
+      <h1>Cocktail List</h1>
+    </div>
+  )
+}
+
+export default CocktailList
---
+++ src/pages/SingleCocktail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SingleCocktail() {
+  return (
+    <div>
+      <h1>Single Cocktail</h1>
+    </div>
+  )
+}
+
+export default SingleCocktail
