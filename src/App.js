cb2f3d4bce1821c3d44c8ac4bd2efe86dbefeece
unknown <anands2807@gmail.com>
2023-08-29 11:13:23 +0530

    react-router setup done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+
+import About from './pages/About';
+import Home from './pages/Home';
+import Error from './pages/Error';
+import SingleCocktail from './pages/SingleCocktail';
+
 
 function App() {
-  return (
-    <div>
-      <h1>Cocktails</h1>     
-    </div>
-  );
+  return(
+    <Routes>
+      <Route exact path="/" element={<Home/>}></Route>
+      <Route exact path="/about" element={<About/>}></Route>
+      <Route exact path="/singlecocktail" element={<SingleCocktail/>}></Route>
+      <Route exact path="*" element={<Error/>}></Route>     
+    </Routes>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import { BrowserRouter } from "react-router-dom";
+
 import { AppProvider } from "./context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AppProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AppProvider>
   </React.StrictMode>
 );
