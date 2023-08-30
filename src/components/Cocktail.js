f5da669a28f801ee063a886e838ab2b9fd9bfd12
unknown <anands2807@gmail.com>
2023-08-30 23:05:30 +0530

    dynamic route added to get to the single cocktail page.

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -10,6 +10,8 @@ function Cocktails({image, id, name, info, glass}) {
       <div className="cocktail-footer">
         <h3>{name}</h3>
         <h4>{glass}</h4>
+        <p>{info}</p>
+        <Link to={`/cocktail/${id}`} className='btn btn-primary btn details'>details</Link>
       </div>
     </article>
   )
