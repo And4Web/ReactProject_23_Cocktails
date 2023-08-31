e7c7bc834ce1a5c129fb764e3e38f0ae6eb51102
unknown <anands2807@gmail.com>
2023-08-31 11:14:55 +0530

    controlled input searchform

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -9,12 +9,16 @@ function SearchForm() {
     searchValue.current.focus();
   }, [])
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   const searchCocktail = () => {
     setSearchTerm(searchValue.current.value);
   }
   return (
     <section className='search section'>
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="name">Search your favorite cocktail</label>
           <input type="text" id='name' ref={searchValue} onChange={searchCocktail} />
